c41cc5361f50145658970a388ec8ae568795c6f0
Ali Nawaz <150611589+Ali90922@users.noreply.github.com>
2024-12-04 16:27:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
-from config_loader import connect_to_database
-from main_menu import main_menu
 from player_menu import player_performance_menu
-from custom_query import handle_custom_query
-from query_executor import execute_query
-
-def list_all_teams(connection):
-    """List all teams in the database."""
-    query = "SELECT team_id, teamName FROM team_info;"
-    try:
-        cursor = connection.cursor(as_dict=True)
-        cursor.execute(query)
-        results = cursor.fetchall()
-        print("+----------------+----------------+")
-        print("| Team ID        | Team Name      |")
-        print("+----------------+----------------+")
-        for row in results:
-            print(f"| {row['team_id']:<14} | {row['teamName']:<14} |")
-        print("+----------------+----------------+")
-    except Exception as e:
-        print("Failed to execute query. Error:", e)
 
 def main():
     """Main function to handle the interface."""
@@ -29,11 +9,7 @@ def main():
     while True:
         choice = main_menu()
         if choice == '1':
-            player_performance_menu()
-        elif choice == '5':
-            list_all_teams(connection)
-        elif choice == '6':
-            handle_custom_query(connection)
+            player_performance_menu(connection)
         elif choice == '7':
             print("Exiting the program. Goodbye!")
             break
@@ -41,7 +17,3 @@ def main():
             print("Feature not implemented yet. Stay tuned!")
 
     connection.close()
-
-if __name__ == "__main__":
-    main()
-
