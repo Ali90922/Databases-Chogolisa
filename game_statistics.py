10f63880ccc9f57f252234d678228d867ade2b9f
Ali Nawaz <150611589+Ali90922@users.noreply.github.com>
2024-12-04 21:32:51 -0600

    Update game_statistics.py

--- game_statistics.py
+++ game_statistics.py
@@ -1,3 +1,9 @@
+
+
+import sys
+sys.path.insert(0, "dependencies")  # Add the dependencies folder to the search path
+
+
 from query_executor import execute_query
 from prettytable import PrettyTable
 
